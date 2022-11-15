be98eca3cdfe6b52ee15f855d4eebed08dd8a91b
Lisa <liza.shoshkina@gmail.com>
2022-11-15 21:20:29 +0100

    Render the WelcomeScreen page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomeScreen from '../../pages/welcome-screen';
+
+type AppProps = {
+  errorsCount: number
+};
+
+function App({errorsCount}: AppProps): JSX.Element {
+  return <WelcomeScreen errorsCount={errorsCount} />;
 }
 
 export default App;
