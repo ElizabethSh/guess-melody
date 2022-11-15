268249b169dcc2d4dbf3107cf4b91bb72ca3bdb7
Lisa <liza.shoshkina@gmail.com>
2022-11-15 21:21:13 +0100

    Pass props to the App component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const ERRORS_COUNT = 3;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      errorsCount={ERRORS_COUNT}
+    />
   </React.StrictMode>,
 );
