50e00fa2155f056d3bb23c0d3326f61ef6681370
Lisa <liza.shoshkina@gmail.com>
2023-02-20 16:25:29 +0100

    Add enums: AppRoute, AuthorizationStatus

---
+++ project/src/settings.ts
@@ -0,0 +1,15 @@
+export enum AppRoute {
+  LOGIN = '/login',
+  LOSE = '/lose',
+  RESULT = '/result',
+  ROOT = '/',
+  // NOTE: Temporary routes
+  ARTIST = '/artist',
+  GENRE = '/genre'
+}
+
+export enum AuthorizationStatus {
+  AUTH = 'auth',
+  NO_AUTH = 'no-auth',
+  UNKNOWN = 'unknown'
+}
