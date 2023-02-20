0728aafb7efdf8ea89af302b2b0301c8b46cc68c
Lisa <liza.shoshkina@gmail.com>
2023-02-20 16:26:59 +0100

    Add the Logo component

---
+++ project/src/components/logo/logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppRoute } from '../../settings';
+
+
+const Logo = () => (
+  <Link className="game__back" to={AppRoute.ROOT}>
+    <span className="visually-hidden">Сыграть ещё раз</span>
+    <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+  </Link>
+);
+
+export default Logo;
