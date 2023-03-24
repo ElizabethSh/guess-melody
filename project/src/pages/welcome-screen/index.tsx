5c6dd60fc0915b862501cab0d5ffb9c8fda1aa9f
Lisa <liza.shoshkina@gmail.com>
2023-03-24 20:33:39 +0100

    Add redirect for the welcome button

--- project/src/pages/welcome-screen/index.tsx
+++ project/src/pages/welcome-screen/index.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { AppRoute } from '../../settings';
 
 type WelcomeScreenProps = {
   errorsCount: number
 };
 
-const WelcomeScreen = ({errorsCount}: WelcomeScreenProps) => (
-  <section className="welcome">
-    <div className="welcome__logo">
-      <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
-    </div>
-    <button className="welcome__button">
-      <span className="visually-hidden">Начать игру</span>
-    </button>
-    <h2 className="welcome__rules-title">Правила игры</h2>
-    <p className="welcome__text">Правила просты:</p>
-    <ul className="welcome__rules-list">
-      <li>Нужно ответить на все вопросы.</li>
-      <li>Можно допустить {errorsCount} ошибки.</li>
-    </ul>
-    <p className="welcome__text">Удачи!</p>
-  </section>
-);
+
+const WelcomeScreen = ({errorsCount}: WelcomeScreenProps): JSX.Element => {
+  const navigate = useNavigate();
+
+  return (
+    <section className="welcome">
+      <div className="welcome__logo">
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
+      </div>
+      <button
+        className="welcome__button"
+        onClick={() => navigate(AppRoute.GAME)}
+      >
+        <span className="visually-hidden">Начать игру</span>
+      </button>
+      <h2 className="welcome__rules-title">Правила игры</h2>
+      <p className="welcome__text">Правила просты:</p>
+      <ul className="welcome__rules-list">
+        <li>Нужно ответить на все вопросы.</li>
+        <li>Можно допустить {errorsCount} ошибки.</li>
+      </ul>
+      <p className="welcome__text">Удачи!</p>
+    </section>
+  );
+};
 
 export default WelcomeScreen;
