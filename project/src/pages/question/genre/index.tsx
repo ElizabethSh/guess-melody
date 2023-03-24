c4dcb004f9d1e61934ade389adff9985a4bcd271
Lisa <liza.shoshkina@gmail.com>
2023-03-24 20:38:25 +0100

    WIP:Add mock to the GenreQuestionScreen

--- project/src/pages/question/genre/index.tsx
+++ project/src/pages/question/genre/index.tsx
@@ -1,75 +1,80 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+
 import Logo from '../../../components/logo/logo';
 
-const GenreQuestion = () => (
-  <section className="game game--genre">
-    <header className="game__header">
-      <Logo />
+import { GenreQuestion, UserGenreQuestionAnswer } from '../../../types/question';
 
-      <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-        <circle className="timer__line" cx="390" cy="390" r="370"
-          style={{ filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center' }}
-        />
-      </svg>
 
-      <div className="game__mistakes">
-        <div className="wrong"></div>
-        <div className="wrong"></div>
-        <div className="wrong"></div>
-      </div>
-    </header>
+type GenreQuestionProps = {
+  question: GenreQuestion;
+  onAnswerClick: (question: GenreQuestion, userAnswers: UserGenreQuestionAnswer ) => void;
+};
 
-    <section className="game__screen">
-      <h2 className="game__title">Выберите инди-рок треки</h2>
-      <form className="game__tracks">
-        <div className="track">
-          <button className="track__button track__button--play" type="button"></button>
-          <div className="track__status">
-            <audio />
-          </div>
-          <div className="game__answer">
-            <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-1" />
-            <label className="game__check" htmlFor="answer-1">Отметить</label>
-          </div>
+
+const GenreQuestionScreen = (props: GenreQuestionProps): JSX.Element => {
+  const {question, onAnswerClick} = props;
+  const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
+
+  const {answers, genre } = question;
+  return (
+    <section className="game game--genre">
+      <header className="game__header">
+        <Logo />
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{ filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center' }}
+          />
+        </svg>
+
+        <div className="game__mistakes">
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+          <div className="wrong"></div>
         </div>
+      </header>
 
-        <div className="track">
-          <button className="track__button track__button--play" type="button"></button>
-          <div className="track__status">
-            <audio />
-          </div>
-          <div className="game__answer">
-            <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-2" />
-            <label className="game__check" htmlFor="answer-2">Отметить</label>
-          </div>
-        </div>
-
-        <div className="track">
-          <button className="track__button track__button--pause" type="button"></button>
-          <div className="track__status">
-            <audio />
-          </div>
-          <div className="game__answer">
-            <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-3" />
-            <label className="game__check" htmlFor="answer-3">Отметить</label>
-          </div>
-        </div>
-
-        <div className="track">
-          <button className="track__button track__button--play" type="button"></button>
-          <div className="track__status">
-            <audio />
-          </div>
-          <div className="game__answer">
-            <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-4" />
-            <label className="game__check" htmlFor="answer-4">Отметить</label>
-          </div>
-        </div>
-
-        <button className="game__submit button" type="submit">Ответить</button>
-      </form>
+      <section className="game__screen">
+        <h2 className="game__title">Выберите {genre} треки</h2>
+        <form
+          className="game__tracks"
+          onSubmit={(evt: FormEvent<HTMLFormElement>) => {
+            evt.preventDefault();
+            onAnswerClick(question, userAnswers);
+          }}
+        >
+          {
+            answers.map((answer, idx) => {
+              const key = `${answer.genre}-${idx}`; // TODO: change it after getting data from server
+              return (
+                <div className="track" key={key}>
+                  <button className="track__button track__button--play" type="button" />
+                  <div className="track__status">
+                    {/* TODO: audio doesn't work yet */}
+                    <audio
+                      src={answer.src}
+                    />
+                  </div>
+                  <div className="game__answer">
+                    <input
+                      className="game__input visually-hidden"
+                      id={`answer-${idx}`}
+                      name="answer"
+                      type="checkbox"
+                      value={answer.genre}
+                      // onChange={}
+                    />
+                    <label className="game__check" htmlFor={`answer-${idx}`}>Отметить</label>
+                  </div>
+                </div>
+              );
+            })
+          }
+          <button className="game__submit button" type="submit">Ответить</button>
+        </form>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
-export default GenreQuestion;
+export default GenreQuestionScreen;
