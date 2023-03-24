15eb79f5afcfa7a9a74d717f4a1b884cc18526cb
Lisa <liza.shoshkina@gmail.com>
2023-03-24 20:39:02 +0100

    Add types for the components

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { AppRoute } from '../../settings';
 
 
-const Logo = () => (
+const Logo = (): JSX.Element => (
   <Link className="game__back" to={AppRoute.ROOT}>
     <span className="visually-hidden">Сыграть ещё раз</span>
     <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
--- project/src/pages/result/lose/index.tsx
+++ project/src/pages/result/lose/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LoseScreen = () => (
+const LoseScreen = (): JSX.Element => (
   <section className="result">
     <div className="result__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
