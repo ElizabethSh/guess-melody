b79ee2426269e1a2644d50344b63c2b641dc00ba
Lisa <liza.shoshkina@gmail.com>
2023-07-14 12:36:50 +0200

    Add types description to the withAudioPlayer HOC

---
+++ project/src/hocs/with-audio-player.tsx
@@ -0,0 +1,37 @@
+import {ComponentType} from 'react';
+import { useState } from 'react';
+import AudioPlayer from '../components/audio-player';
+
+type withAudioPlayerProps = {
+  renderPlayer: (src: string, idx: number) => void;
+}
+
+
+const withAudioPlayer = <T extends object>(Component: ComponentType<T>): ComponentType<Omit<T, keyof withAudioPlayerProps>> => {
+
+  type ComponentProps = Omit<T, keyof withAudioPlayerProps>
+
+  const WithAudioPlayer = (props: ComponentProps): JSX.Element => {
+    const [activePlayer, setActivePlayer] = useState(-1);
+
+    return (
+      <Component
+        {...props as T}
+        renderPlayer = {(src: 'string', idx: 'number') => {
+          const index = Number(idx);
+          return (
+            <AudioPlayer
+              isPlaying={index === activePlayer}
+              onPlayAudioClick={() => setActivePlayer(index === activePlayer ? -1 : index)}
+              src={src}
+            />
+          );
+        }}
+      />
+    );
+  };
+
+  return WithAudioPlayer;
+};
+
+export default withAudioPlayer;
