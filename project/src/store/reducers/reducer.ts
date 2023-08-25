b20b3b5c14fb8a4d5ab20ff3612e2466cbaee405
Lisa <liza.shoshkina@gmail.com>
2023-08-25 15:24:03 +0200

    Add case for the resetGame action

--- project/src/store/reducers/reducer.ts
+++ project/src/store/reducers/reducer.ts
@@ -1,19 +1,25 @@
 import { createReducer } from "@reduxjs/toolkit";
-import { incrementStep } from "../actions/game";
+import { incrementStep, resetGame } from "../actions/game";
 
 import { FIRST_GAME_STEP } from './../../settings';
 
 const STEP_GAP = 1;
 
 const initialState = {
-  mistakes: 0,
+  mistakesCount: 0,
   step: FIRST_GAME_STEP
 };
 
 const reducer = createReducer(initialState, (builder) => {
+
   builder
-    .addCase(incrementStep, (state) => {
+  .addCase(incrementStep, (state) => {
+      console.log('builder');
       state.step = state.step + STEP_GAP;
+    })
+    .addCase(resetGame, (state) => {
+      state.mistakesCount = 0;
+      state.step = FIRST_GAME_STEP;
     })
 });
 
