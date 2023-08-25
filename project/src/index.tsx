0755e0d7b4851e37b95056eb99dbd3b95585f80a
Lisa <liza.shoshkina@gmail.com>
2023-08-25 14:04:59 +0200

    Add redux to the project

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './components/app/app';
 
 import { AuthorizationStatus, MAX_ERRORS_COUNT } from './settings';
 import {questions} from './mocks/questions';
+import store from './store';
 
 
 const root = ReactDOM.createRoot(
@@ -13,10 +15,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      authorizationStatus={AuthorizationStatus.AUTH}
-      errorsCount={MAX_ERRORS_COUNT}
-      questions={questions}
-    />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App
+        authorizationStatus={AuthorizationStatus.AUTH}
+        errorsCount={MAX_ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>
+  </React.StrictMode>
 );
