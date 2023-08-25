1a976ef62aca27ad7b66c75e5c4d2a1e68af7825
Lisa <liza.shoshkina@gmail.com>
2023-08-25 15:23:29 +0200

    Add the resetGame action

--- project/src/store/actions/game.ts
+++ project/src/store/actions/game.ts
@@ -1,3 +1,4 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const incrementStep = createAction('game/incrementStep')
+export const incrementStep = createAction('game/incrementStep');
+export const resetGame = createAction('game/reset');
