7d2e1a45c337942e02dec44c9e35e764fc5bdf65
Lisa <liza.shoshkina@gmail.com>
2023-08-25 15:17:18 +0200

    Update the Mistakes component

--- project/src/components/mistakes/mistakes.tsx
+++ project/src/components/mistakes/mistakes.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useAppSelector } from '../../hooks';
 
-const Mistakes = () => (
-  <div className="game__mistakes">
-    <div className="wrong"></div>
-    <div className="wrong"></div>
-    <div className="wrong"></div>
-  </div>
-);
+const Mistakes = () => {
+  const mistakesCount = useAppSelector((state) => state.mistakesCount);
+  const mistakes = Array.from(Array(mistakesCount).keys());
+
+  return (
+    <div className="game__mistakes">
+      {!!mistakes.length && mistakes.map((mistake, index) => (<div className="wrong" key={index}></div>))}
+    </div>
+  );
+
+};
 
 export default Mistakes;
