6ba405d12730edf4e118958d78fd7196cf3d7f3e
Lisa <liza.shoshkina@gmail.com>
2023-08-25 15:18:01 +0200

    Add types for redux hooks

---
+++ project/src/hooks/index.ts
@@ -0,0 +1,7 @@
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import { AppDispatch, State } from '../types/state';
+
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
