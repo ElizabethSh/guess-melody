0c074fd331d30e5cf3b8b61dd433bfc693d2cc85
Lisa <liza.shoshkina@gmail.com>
2023-11-17 13:29:24 +0100

    Add utils to get/set token from/to localStorage

---
+++ project/src/servises/token.ts
@@ -0,0 +1,23 @@
+const LOCALSTORAGE_AUTH_TOKEN = 'auth-token';
+
+export type Token = string;
+
+export const getToken = (): Token => {
+  let token = '';
+  try {
+    if (global.localStorage && (typeof global.localStorage.getItem) === 'function') {
+      token = JSON.parse(localStorage.getItem(LOCALSTORAGE_AUTH_TOKEN) || '');
+    }
+
+  } catch (err) { }
+  return token;
+};
+
+export const setToken = (token: Token) => {
+  try {
+    if (global.localStorage && (typeof global.localStorage.setItem) === 'function') {
+      localStorage.setItem(LOCALSTORAGE_AUTH_TOKEN, token);
+    }
+
+  } catch (err) { }
+};
