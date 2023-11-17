4a53a38f4fc83cab2e8c1781ee599ff6d6281fa0
Lisa <liza.shoshkina@gmail.com>
2023-11-17 16:14:12 +0100

    Fix error: add types

--- project/src/store/actions/game.ts
+++ project/src/store/actions/game.ts
@@ -1,6 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
+import { AuthorizationStatus, Questions } from '../../types/question';
 
 export const incrementMistakes = createAction('game/incrementMistakes');
 export const incrementStep = createAction('game/incrementStep');
-export const loadQuestions = createAction('data/loadQuestions');
+export const loadQuestions = createAction<Questions>('data/loadQuestions');
+export const requireAuthorisation = createAction<AuthorizationStatus>('user/requireAuthorisation');
 export const resetGame = createAction('game/reset');
