5f0b9f1420001b0bd6d20c51607d91b43c0959d2
Lisa <liza.shoshkina@gmail.com>
2023-11-17 15:19:53 +0100

    WIP: add case to load questions

--- project/src/store/reducers/reducer.ts
+++ project/src/store/reducers/reducer.ts
@@ -1,6 +1,12 @@
-import { incrementMistakes } from './../actions/game';
 import { createReducer } from '@reduxjs/toolkit';
-import { incrementStep, resetGame } from '../actions/game';
+import { questions } from '../../mocks/questions';
+
+import {
+  incrementMistakes,
+  loadQuestions,
+  incrementStep,
+  resetGame
+} from './../actions/game';
 
 import { FIRST_GAME_STEP } from './../../settings';
 
@@ -8,6 +14,7 @@ const STEP_GAP = 1;
 
 const initialState = {
   mistakesCount: 0,
+  questions,
   step: FIRST_GAME_STEP
 };
 
@@ -23,6 +30,9 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(incrementMistakes, (state) => {
       state.mistakesCount = state.mistakesCount + 1;
+    })
+    .addCase(loadQuestions, (state, action) => {
+      state.questions = action.payload;
     });
 });
 
