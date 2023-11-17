b02c232c34d12e4b45594067d97303cc15df8ab0
Lisa <liza.shoshkina@gmail.com>
2023-11-17 11:48:50 +0100

    Add Axios config

---
+++ project/src/servises/api.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosInstance } from 'axios';
+
+const BASE_URL = ' https://10.react.pages.academy/guess-melody';
+const REQUEST_TIMEOUT = 5000;
+
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  return api;
+};
