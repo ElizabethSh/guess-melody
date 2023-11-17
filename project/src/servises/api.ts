405259b7d77e2d5c27f54dd191572fdb6ac59663
Lisa <liza.shoshkina@gmail.com>
2023-11-17 15:09:14 +0100

    WIP: add interceptor to axios-config to set auth-token

--- project/src/servises/api.ts
+++ project/src/servises/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BASE_URL = ' https://10.react.pages.academy/guess-melody';
 const REQUEST_TIMEOUT = 5000;
@@ -9,6 +10,17 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BASE_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
 
   return api;
 };
