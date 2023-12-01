75e154677d48466941be8e5c054416b46e4a1e4a
Lisa <liza.shoshkina@gmail.com>
2023-12-01 14:16:36 +0100

    Add navigation to the game-page

--- project/src/pages/result/lose/index.tsx
+++ project/src/pages/result/lose/index.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../../settings';
+import { resetGame } from '../../../store/actions/game';
+
 
 const LoseScreen = (): JSX.Element => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onReplyClick = () => {
+    dispatch(resetGame())
+    navigate(AppRoute.GAME)
+  };
 
   return (
     <section className="result">
       <div className="result__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
       </div>
-      <h2 className="result__title">Какая жалость!</h2>
+      <h2 className="result__title">What a pity!</h2>
       <p className="result__total result__total--fail">
-        У вас закончились все попытки. Ничего, повезёт в следующий раз!
+        You've run out of attempts. Never mind, you'll be lucky next time!
       </p>
       <button
         className="replay"
         type="button"
-        onClick={() => navigate(AppRoute.ROOT)}
+        onClick={onReplyClick}
       >
-        Попробовать ещё раз
+        Try again
       </button>
     </section>
   );
--- project/src/pages/result/win/index.tsx
+++ project/src/pages/result/win/index.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { AppRoute } from '../../../settings';
+import { resetGame } from '../../../store/actions/game';
+import { state } from '../../../types/state';
 
 
-const WinScreen = (): JSX.Element => (
-  <section className="result">
-    <div className="result-logout__wrapper">
-      <Link className="result-logout__link" to={AppRoute.ROOT}>Выход</Link>
-    </div>
-    <div className="result__logo">
-      <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
-    </div>
-    <h2 className="result__title">Вы настоящий меломан!</h2>
-    <p className="result__total">
-      Вы ответили правильно на 6 вопросов и совершили 2 ошибки
-    </p>
-    <button className="replay" type="button">Сыграть ещё раз</button>
-  </section>
-);
+const WinScreen = (): JSX.Element => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const mistakesCount = useSelector((state: state) => state.mistakesCount);
+  const questions = useSelector((state: state) => state.questions);
+
+  const onReplyClick = () => {
+    dispatch(resetGame())
+    navigate(AppRoute.GAME)
+  };
+
+  return (
+    <section className="result">
+      <div className="result-logout__wrapper">
+        <Link className="result-logout__link" to={AppRoute.ROOT}>Log out</Link>
+      </div>
+      <div className="result__logo">
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
+      </div>
+      <h2 className="result__title">You are a real music lover!</h2>
+      <p className="result__total">
+        You answered {questions.length - mistakesCount} questions correctly and made {mistakesCount} mistakes
+      </p>
+      <button
+        className="replay"
+        onClick={onReplyClick}
+        type="button"
+      >
+        Play again
+      </button>
+    </section>
+  )
+};
 
 export default WinScreen;
