bd2941ce48129ec5150088a65d50a2380ec89a87
Lisa <liza.shoshkina@gmail.com>
2023-12-01 15:12:09 +0100

    Add link to logo

--- project/src/pages/result/win/index.tsx
+++ project/src/pages/result/win/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
 
 import { AppRoute } from '../../../settings';
 import { resetGame } from '../../../store/actions/game';
@@ -10,9 +9,9 @@ import { state } from '../../../types/state';
 
 const WinScreen = (): JSX.Element => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const mistakesCount = useSelector((state: state) => state.mistakesCount);
-  const questions = useSelector((state: state) => state.questions);
+  const dispatch = useAppDispatch();
+  const mistakesCount = useAppSelector((state: state) => state.mistakesCount);
+  const questions = useAppSelector((state: state) => state.questions);
 
   const onReplyClick = () => {
     dispatch(resetGame())
@@ -24,9 +23,9 @@ const WinScreen = (): JSX.Element => {
       <div className="result-logout__wrapper">
         <Link className="result-logout__link" to={AppRoute.ROOT}>Log out</Link>
       </div>
-      <div className="result__logo">
+      <Link className="result__logo" to={AppRoute.ROOT}>
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
-      </div>
+      </Link>
       <h2 className="result__title">You are a real music lover!</h2>
       <p className="result__total">
         You answered {questions.length - mistakesCount} questions correctly and made {mistakesCount} mistakes
