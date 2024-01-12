46f25e86ea82dc0f628e8491f69da0c383255524
Lisa <liza.shoshkina@gmail.com>
2024-01-12 14:26:41 +0100

    Add logout function

--- project/src/pages/result/win/index.tsx
+++ project/src/pages/result/win/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
 
-import { AppRoute } from '../../../settings';
+import { AppRoute, AuthorizationStatus } from '../../../settings';
+import { logoutAction } from '../../../store/actions/api-actions';
 import { resetGame } from '../../../store/actions/game';
 import { state } from '../../../types/state';
 
@@ -12,19 +13,34 @@ const WinScreen = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const mistakesCount = useAppSelector((state: state) => state.mistakesCount);
   const questions = useAppSelector((state: state) => state.questions);
+  const authorizationStatus = useAppSelector((state: state) => state.authorizationStatus);
 
   const onReplyClick = () => {
     dispatch(resetGame())
     navigate(AppRoute.GAME)
   };
 
+  const onLogoutClick = (evt: React.MouseEvent<HTMLElement>) => {
+    evt.preventDefault();
+    dispatch(logoutAction());
+  }
+
   return (
     <section className="result">
-      <div className="result-logout__wrapper">
-        <Link className="result-logout__link" to={AppRoute.ROOT}>Log out</Link>
-      </div>
+      {
+        authorizationStatus === AuthorizationStatus.AUTH
+          && (
+            <div className="result-logout__wrapper">
+              <Link
+                className="result-logout__link"
+                onClick={(evt: React.MouseEvent<HTMLElement>) => onLogoutClick(evt)}
+                to={AppRoute.ROOT}
+              >Log out</Link>
+            </div>
+          )
+      }
       <Link className="result__logo" to={AppRoute.ROOT}>
-        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
+        <img src="img/melody-logo.png" alt="Guess melody" width="186" height="83" />
       </Link>
       <h2 className="result__title">You are a real music lover!</h2>
       <p className="result__total">
