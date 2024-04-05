27bb009c9be705b324211ca8ac6ee1e773d43079
Lisa <liza.shoshkina@gmail.com>
2024-04-05 16:34:13 +0200

    Add the ToastNotifications component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './components/app/app';
+import ToastNotifications from './components/toast-notifications';
 
 import { MAX_ERRORS_COUNT } from './settings';
 import store from './store';
@@ -19,9 +20,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App
-        errorsCount={MAX_ERRORS_COUNT}
-      />
+      <ToastNotifications />
+      <App errorsCount={MAX_ERRORS_COUNT} />
     </Provider>
   </React.StrictMode>
 );
