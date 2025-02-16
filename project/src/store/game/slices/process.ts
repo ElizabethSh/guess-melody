bd506392c460b4ea4012a6edf5c4580ecbc82c77
Lisa <liza.shoshkina@gmail.com>
2025-02-16 12:58:10 +0100

    Add game slice

---
+++ project/src/store/game/slices/process.ts
@@ -0,0 +1,32 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+import {NameSpace, FIRST_GAME_STEP} from '../../../settings';
+import { GameProcess } from '../../../types/state';
+import { isAnswerCorrect } from '../../../utils';
+
+const initialState: GameProcess = {
+  mistakes: 0,
+  step: FIRST_GAME_STEP,
+};
+
+const STEP_COUNT = 1;
+
+export const gameProcess = createSlice({
+  name: NameSpace.Game,
+  initialState,
+  reducers: {
+    incrementStep: (state) => {
+      state.step = state.step + STEP_COUNT;
+    },
+    checkUserAnswer: (state, action) => {
+      const {question, userAnswer} = action.payload;
+      state.mistakes += Number(!isAnswerCorrect(question, userAnswer));
+    },
+    resetGame: (state) => {
+      state.mistakes = 0;
+      state.step = FIRST_GAME_STEP;
+    },
+  },
+});
+
+export const {incrementStep, checkUserAnswer, resetGame} = gameProcess.actions;
