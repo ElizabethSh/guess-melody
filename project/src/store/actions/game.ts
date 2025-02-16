183f86c9d1f85c6de2411bce83c087460cc26d07
Lisa <liza.shoshkina@gmail.com>
2025-02-16 12:49:19 +0100

    Cleanup

--- project/src/store/actions/game.ts
+++ project/src/store/actions/game.ts
@@ -1,12 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 import { AppRoute } from '../../settings';
-import { AuthorizationStatus, Questions } from '../../types/question';
+import { AuthorizationStatus } from '../../types/user';
 
 export const incrementMistakes = createAction('game/incrementMistakes');
-export const incrementStep = createAction('game/incrementStep');
-export const loadQuestions = createAction<Questions>('data/loadQuestions');
 export const requireAuthorisation = createAction<AuthorizationStatus>('user/requireAuthorisation');
-export const resetGame = createAction('game/reset');
 export const setError = createAction<string>('data/setError');
 export const clearError = createAction<string[]>('data/clearError');
 
