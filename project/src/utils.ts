83552dd5bec73985509383e494325b50d927a05f
Lisa <liza.shoshkina@gmail.com>
2025-02-16 13:00:37 +0100

    Add utils

---
+++ project/src/utils.ts
@@ -0,0 +1,31 @@
+import {
+  Question,
+  UserArtistQuestionAnswer,
+  UserGenreQuestionAnswer,
+  UserAnswer,
+  ArtistQuestion,
+  GenreQuestion
+} from './types/question';
+import {GameType, AuthorizationStatus} from './settings';
+
+export const isAnswerCorrect = (question: Question, answer: UserAnswer) => {
+  if (question.type === GameType.Artist && typeof answer === 'string') {
+    return isArtistAnswerCorrect(question, answer);
+  }
+
+  if (question.type === GameType.Genre && Array.isArray(answer)) {
+    return isGenreAnswerCorrect(question, answer);
+  }
+
+  return false;
+};
+
+export const isArtistAnswerCorrect = (question: ArtistQuestion, userAnswer: UserArtistQuestionAnswer) =>
+  userAnswer === question.song.artist;
+
+export const isGenreAnswerCorrect = (question: GenreQuestion, userAnswer: UserGenreQuestionAnswer) =>
+  userAnswer.every((answer, index) =>
+    answer === (question.answers[index].genre === question.genre));
+
+export const isCheckedAuth = (authorizationStatus: AuthorizationStatus) =>
+  authorizationStatus === AuthorizationStatus.Unknown;
