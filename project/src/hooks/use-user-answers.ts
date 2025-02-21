cfdd11763d7e9a67d4ffc8fde1a1dd8493acb769
Lisa <liza.shoshkina@gmail.com>
2025-02-21 13:21:30 +0100

    Add useUserAnswers hook

---
+++ project/src/hooks/use-user-answers.ts
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+import {GenreQuestion} from '../types/question';
+
+type ResultUserAnswers = [boolean[], (id: number, value: boolean) => void];
+
+export const useUserAnswers = (question: GenreQuestion): ResultUserAnswers => {
+  const answersCount = question.answers.length;
+
+  const [answers, setAnswers] = useState<boolean[]>(Array.from({length: answersCount}, () => false));
+
+  const handleAnswerChange = (id: number, value: boolean) => {
+    const userAnswers = answers.slice(0);
+    userAnswers[id] = value;
+    setAnswers(userAnswers);
+  };
+
+  return [answers, handleAnswerChange];
+};
