4654ae0d89e616c588d0e0fda2ecc01e59b3b01e
Lisa <liza.shoshkina@gmail.com>
2025-02-28 14:08:31 +0100

    Add test for the Loader component

---
+++ project/src/components/loader/loader.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Loader from ".";
+
+describe('Component: Loader', () => {
+  test('should render correctly', () => {
+    render(<Loader />);
+
+    expect(screen.getByText("Loading...")).toBeVisible();
+  });
+});
