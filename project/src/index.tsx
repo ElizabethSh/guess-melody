fffcf408ecbba0a91fdb1989d0483833baaabd95
Lisa <liza.shoshkina@gmail.com>
2025-02-28 14:08:04 +0100

    Refactoring

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './components/app/app';
@@ -13,7 +13,7 @@ store.dispatch(fetchQuestionAction());
 store.dispatch(checkAuthAction());
 
 
-const root = ReactDOM.createRoot(
+const root = createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
