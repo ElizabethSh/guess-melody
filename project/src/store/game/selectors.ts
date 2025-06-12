59f58f557c3c91267ac53a9f79e186beb54375b8
Lisa <liza.shoshkina@gmail.com>
2025-06-12 09:33:19 +0200

    Remove files

--- project/src/store/game/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { NameSpace } from '../../settings';
-import { State } from '../../types/state';
-
-// game process
-export const selectStep = (state: State) => state[NameSpace.Game].step;
-export const selectMistakeCount = (state: State) =>
-  state[NameSpace.Game].mistakes;
