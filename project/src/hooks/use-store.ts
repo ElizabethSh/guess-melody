3c8c75c68a4a4b197ced6d63a7a2c042dac6bfcd
Lisa <liza.shoshkina@gmail.com>
2025-07-02 21:24:12 +0200

    Rename hooks/index.ts to hooks/use-store.ts

--- project/src/hooks/use-store.ts
+++ project/src/hooks/use-store.ts
@@ -1,6 +1,6 @@
-import { TypedUseSelectorHook,useDispatch, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-import { AppDispatch, State } from '../types/state';
+import { AppDispatch, State } from 'types/state';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
