a0f866966f05e342bb8eecbbd2a33bbaf34c58d6
Lisa Shoshkina <liza.shoshkina@gmail.com>
2025-07-08 16:07:45 +0200

    Feat: update not-found page (#43)

--- project/src/pages/not-found/index.tsx
+++ project/src/pages/not-found/index.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '@components/logo/logo';
+import Layout from '@components/layout';
 import { AppRoute } from '@settings';
 
 import './not-found.css';
 
 const NotFoundPage: React.FC = () => (
-  <section className="game">
-    <header className="game__header">
-      <Logo />
-    </header>
-    <main className="game__screen not-found">
-      <h1 className="not-found__title">404</h1>
-      <h2 className="not-found__subtitle">Page not found</h2>
-      <p className="not-found__description">
-        Sorry, the page you are looking for doesn’t exist or has been moved.
-      </p>
-      <Link className="link" to={AppRoute.Root}>
-        Go back to the main page
-      </Link>
-    </main>
-  </section>
+  <Layout className="not-found">
+    <h1 className="not-found__title">404</h1>
+    <h2 className="not-found__subtitle">Page not found</h2>
+    <p className="not-found__description">
+      Sorry, the page you are looking for doesn’t exist or has been moved.
+    </p>
+    <Link className="link" to={AppRoute.Root}>
+      Go back to the main page
+    </Link>
+  </Layout>
 );
 
 export default NotFoundPage;
