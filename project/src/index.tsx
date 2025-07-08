9caf42781d0e7ab400f56f37388d8279bc0d823d
Lisa <liza.shoshkina@gmail.com>
2025-07-08 11:28:36 +0200

    Wrap the App in the AppInitializer component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@components/app/app';
+import AppInitializer from '@components/app-initializer';
 import ToastNotifications from '@components/toast-notifications';
 import { checkAuthAction, fetchQuestionAction } from '@store/api-actions';
 
@@ -10,6 +11,7 @@ import store from './store';
 
 import './styles.css';
 
+// Initialize the app by fetching required data
 store.dispatch(fetchQuestionAction());
 store.dispatch(checkAuthAction());
 
@@ -19,8 +21,10 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
-        <ToastNotifications />
+        <AppInitializer>
+          <App />
+          <ToastNotifications />
+        </AppInitializer>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
