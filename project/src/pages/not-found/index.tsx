55ae3b7ca4a510e458272716fcb4027119c41eaa
Lisa <liza.shoshkina@gmail.com>
2025-07-09 10:36:56 +0200

    Update not-found page

--- project/src/pages/not-found/index.tsx
+++ project/src/pages/not-found/index.tsx
@@ -8,11 +8,11 @@ import './not-found.css';
 const NotFoundPage: React.FC = () => (
   <Layout className="not-found">
     <h1 className="not-found__title">404</h1>
-    <h2 className="not-found__subtitle">Page not found</h2>
+    <h2 className="main__title">Page not found</h2>
     <p className="not-found__description">
       Sorry, the page you are looking for doesn’t exist or has been moved.
     </p>
-    <Link className="link" to={AppRoute.Root}>
+    <Link className="link not-found__link" to={AppRoute.Root}>
       Go back to the main page
     </Link>
   </Layout>
