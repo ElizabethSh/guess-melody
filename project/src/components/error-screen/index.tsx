6497273120d2b9ffbd46b5d2611eec8b7232028c
Lisa <liza.shoshkina@gmail.com>
2025-07-14 16:07:51 +0200

    Add types to tests

--- project/src/components/error-screen/index.tsx
+++ project/src/components/error-screen/index.tsx
@@ -4,7 +4,7 @@ import Layout from '@components/layout';
 
 import './error-screen.css';
 
-type ErrorScreenProps = {
+export type ErrorScreenProps = {
   message?: string;
   onRetry?: () => void;
 };
